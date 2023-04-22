73858d95cfc7dddb711a8750197a0e74b1a518cd
Nishant Pandya <nishant.pandya@azilen.com>
2023-04-22 10:59:35 +0530

    Add project files.

---
+++ PwaApp/Controllers/AbountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PwaApp.Models;
+
+namespace PwaApp.Controllers
+{      
+    [Route("[controller]")]
+    public class AbountController : Controller
+    {
+        [Route("[action]")]
+        public string Phone()
+        {
+            return "+9157156699";
+        }
+
+
+        [Route("[action]")]
+        public ViewResult Emp()
+        {
+            var emp = new Employee { Id = 1, Name = "df" };
+            return View(emp);
+        }
+        /*        [Route("[action]")]
+        public ObjectResult Emp()
+        {
+            var emp = new Employee { Id = 1, Name = "df" };
+            return new ObjectResult(emp);
+        }*/
+
+    }
+}
